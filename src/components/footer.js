2b98bf5b8eddcbb213cfc6e1cb34259e88080baa
Alif Abhiesa <abhiesa24@gmail.com>
2020-01-29 11:30:31 +0700

    ok

--- src/components/footer.js
+++ src/components/footer.js
@@ -124,7 +124,7 @@ const Footer = () => {
       </StyledSocial>
       <StyledMetadata tabindex="-1">
         <StyledGitHubLink
-          href="https://github.com/bchiang7/v4"
+          href="https://github.com/AlifAbhiesa"
           target="_blank"
           rel="nofollow noopener noreferrer">
           {/* <div>Designed &amp; Built by Brittany Chiang</div> */}
